8ee10566460f5e20f2422e5ee0d27908fd408942
Otavio Cartaxo <oacartaxo@gmail.com>
2025-09-15 00:57:05 -0300

    refactor: add constants

--- src/main/java/br/com/alura/AluraFake/task/Type.java
+++ src/main/java/br/com/alura/AluraFake/task/Type.java
@@ -1,7 +1,19 @@
 package br.com.alura.AluraFake.task;
 
 public enum Type {
-    OPEN_TEXT,
-    MULTIPLE_CHOICE,
-    SINGLE_CHOICE;
+    OPEN_TEXT(Constants.OPEN_TEXT),
+    MULTIPLE_CHOICE(Constants.MULTIPLE_CHOICE),
+    SINGLE_CHOICE(Constants.SINGLE_CHOICE);
+
+    public String value;
+
+    Type(String value) {
+        this.value = value;
+    };
+
+    public static class Constants {
+        public static final String OPEN_TEXT = "OPEN_TEXT";
+        public static final String MULTIPLE_CHOICE = "MULTIPLE_CHOICE";
+        public static final String SINGLE_CHOICE = "SINGLE_CHOICE";
+    }
 }
