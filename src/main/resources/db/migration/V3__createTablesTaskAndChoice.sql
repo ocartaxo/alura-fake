9466d338cb45506d97cd1433a5f8edd4edf07824
Otavio Cartaxo <oacartaxo@gmail.com>
2025-09-15 00:57:47 -0300

    feat: migration script for create new tables

---
+++ src/main/resources/db/migration/V3__createTablesTaskAndChoice.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Task(
+     id bigint(20) NOT NULL AUTO_INCREMENT,
+     task_type enum('OPEN_TEXT', 'SINGLE_CHOICE', 'MULTIPLE_CHOICE') CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci NOT NULL,
+     statement varchar(255) NOT NULL,
+     `order` int NOT NULL,
+     course_id bigint(20) NOT NULL,
+     createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+     PRIMARY KEY (id),
+     CONSTRAINT UC_Course_Statement UNIQUE (course_id, statement),
+     CONSTRAINT FK_Activity_Course FOREIGN KEY (course_id) REFERENCES Course(id) ON DELETE CASCADE
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+
+
+CREATE TABLE Choice(
+     id bigint(20) NOT NULL AUTO_INCREMENT,
+     text varchar(80) NOT NULL,
+     is_correct boolean NOT NULL,
+     task_id bigint(20) NOT NULL,
+     createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+     PRIMARY KEY (id),
+     CONSTRAINT FK_Option_Activity FOREIGN KEY (task_id) REFERENCES Task(id) ON DELETE CASCADE
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
