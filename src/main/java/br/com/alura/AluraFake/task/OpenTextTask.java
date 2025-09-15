cbbe06ec17c9a34c8120a2146f051239ea593c4b
Otavio Cartaxo <oacartaxo@gmail.com>
2025-09-15 17:24:03 -0300

    fix: open task entity constructor

--- src/main/java/br/com/alura/AluraFake/task/OpenTextTask.java
+++ src/main/java/br/com/alura/AluraFake/task/OpenTextTask.java
@@ -1,5 +1,6 @@
 package br.com.alura.AluraFake.task;
 
+import br.com.alura.AluraFake.course.Course;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
@@ -11,7 +12,7 @@ public class OpenTextTask extends Task {
     public OpenTextTask() {
     }
 
-    public OpenTextTask(String statement, int order) {
-        super(statement, order);
+    public OpenTextTask(String statement, int order, Course course) {
+        super(statement, order, course);
     }
 }
