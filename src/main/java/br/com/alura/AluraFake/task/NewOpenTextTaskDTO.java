0ddb318dc74af6949aac0db41338f5047b073a8f
Otavio Cartaxo <oacartaxo@gmail.com>
2025-09-15 17:24:36 -0300

    feat: add method to create entity class from dto

--- src/main/java/br/com/alura/AluraFake/task/NewOpenTextTaskDTO.java
+++ src/main/java/br/com/alura/AluraFake/task/NewOpenTextTaskDTO.java
@@ -1,13 +1,27 @@
 package br.com.alura.AluraFake.task;
 
+import br.com.alura.AluraFake.course.Course;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
-public record NewOpenTextTask(
+import java.util.Collections;
+import java.util.List;
+
+public record NewOpenTextTaskDTO(
         @Positive(message = "A ordem da tarefa deve ser um número positivo")
         int order,
         Long courseId,
         @Size(min = 4, max = 255, message = "O enunciado da tarefa possui quantidade de caracteres inválidos")
         String statement
-) {
+) implements TaskWithChoices {
+
+    @Override
+    public Task toModel(Course course) {
+        return new OpenTextTask(statement, order, course);
+    }
+
+    @Override
+    public List<ChoiceDTO> options() {
+        return Collections.emptyList();
+    }
 }
