0e586e3dc9f2a87b8045addaca8354e9b372f343
Otavio Cartaxo <oacartaxo@gmail.com>
2025-09-16 13:55:47 -0300

    feat: add methods to save new course

--- src/main/java/br/com/alura/AluraFake/course/SaveCourseService.java
+++ src/main/java/br/com/alura/AluraFake/course/SaveCourseService.java
@@ -1,13 +1,19 @@
 package br.com.alura.AluraFake.course;
 
+import br.com.alura.AluraFake.user.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SaveCourseService {
     private final  CourseRepository courseRepository;
+
     public SaveCourseService(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
     }
 
+    public void save(NewCourseDTO newCourse, User author) {
+        Course course = new Course(newCourse.getTitle(), newCourse.getDescription(), author);
+        courseRepository.save(course);
+    }
 
 }
