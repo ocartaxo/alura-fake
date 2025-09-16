19cc28e9f1f06ebe51b3ac2315fa6de050cb75c2
Otavio Cartaxo <oacartaxo@gmail.com>
2025-09-16 16:31:32 -0300

    feat: handle validation exceptions

--- src/main/java/br/com/alura/AluraFake/util/ValidationExceptionHandler.java
+++ src/main/java/br/com/alura/AluraFake/util/ValidationExceptionHandler.java
@@ -2,6 +2,8 @@ package br.com.alura.AluraFake.util;
 
 import br.com.alura.AluraFake.course.CourseValidationException;
 import br.com.alura.AluraFake.task.TaskValidationException;
+import br.com.alura.AluraFake.user.NotInstructorException;
+import br.com.alura.AluraFake.user.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -34,5 +36,19 @@ public class ValidationExceptionHandler {
     public ResponseEntity<List<ErrorItemDTO>> handleValidationExceptions(CourseValidationException ex) {
         final var error =  new ErrorItemDTO(ex.getField(), ex.getMessage());
         return ResponseEntity.badRequest().body(Collections.singletonList(error));
+    }
+
+    @ExceptionHandler(NotInstructorException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<List<ErrorItemDTO>> handleValidationExceptions(NotInstructorException ex) {
+        final var error =  new ErrorItemDTO(ex.getField(), ex.getMessage());
+        return ResponseEntity.badRequest().body(Collections.singletonList(error));
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<List<ErrorItemDTO>> handleValidationExceptions(UserNotFoundException ex) {
+        final var error =  new ErrorItemDTO(ex.getMessage(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonList(error));
     }
 }
