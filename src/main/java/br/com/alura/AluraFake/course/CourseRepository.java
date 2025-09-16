b2441f4f6f1d90f7265acfb2968bf067e4a1a3ab
Otavio Cartaxo <oacartaxo@gmail.com>
2025-09-16 00:52:13 -0300

    feat: add method to find course status by id

--- src/main/java/br/com/alura/AluraFake/course/CourseRepository.java
+++ src/main/java/br/com/alura/AluraFake/course/CourseRepository.java
@@ -3,5 +3,5 @@ package br.com.alura.AluraFake.course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<Course, Long>{
-
+    Status findStatusById(Long id);
 }
