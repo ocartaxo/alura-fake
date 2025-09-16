29cb87cee2a31eefd13c298ca21f92a5f3694647
Otavio Cartaxo <oacartaxo@gmail.com>
2025-09-15 21:08:26 -0300

    refactor: remove empty line

--- src/main/java/br/com/alura/AluraFake/task/SingleChoiceTask.java
+++ src/main/java/br/com/alura/AluraFake/task/SingleChoiceTask.java
@@ -16,12 +16,27 @@ public class SingleChoiceTask extends Task {
     @OneToMany(mappedBy = "task", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Choice> choices = new ArrayList<>();
 
+    private static final int CHOICES_LIMIT = 5;
+
     public SingleChoiceTask() {
     }
 
     public SingleChoiceTask(String statement, int order, Course course, List<Choice> choices) {
         super(statement, order, course);
-        this.choices = choices;
+        this.choices.addAll(choices);
+        choices.forEach(choice -> choice.setTask(this));
+    }
+
+    public void addChoice(Choice choice) {
+        if(choices.size() >= CHOICES_LIMIT){
+            throw new IllegalStateException("Limie de opções atingidos");
+        }
+
+        this.choices.add(choice);
+    }
+
+    public List<Choice> getChoices() {
+        return choices;
     }
 
 }
