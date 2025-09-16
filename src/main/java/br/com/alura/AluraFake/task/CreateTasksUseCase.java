4c4cb230ce14efe03899e8dea29a46350a374029
Otavio Cartaxo <oacartaxo@gmail.com>
2025-09-16 00:54:34 -0300

    refactor: add check for task with same order

--- src/main/java/br/com/alura/AluraFake/task/CreateTasksUseCase.java
+++ src/main/java/br/com/alura/AluraFake/task/CreateTasksUseCase.java
@@ -1,5 +1,6 @@
 package br.com.alura.AluraFake.task;
 
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -8,28 +9,43 @@ import org.springframework.stereotype.Component;
 @Component
 public class CreateTasksUseCase {
 
-    private final SaveNewTaskService saveNewTaskService;
+    private final SaveTaskService saveTaskService;
+
+    private final ExistsTaskService existsTaskService;
 
     private final TaskValidatorStrategyFactory validatorStrategyFactory;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CreateTasksUseCase.class);
 
     public CreateTasksUseCase(
-            SaveNewTaskService saveNewTaskService,
+            SaveTaskService saveTaskService,
+            ExistsTaskService existsTaskService,
             TaskValidatorStrategyFactory validatorStrategyFactory
     ) {
-        this.saveNewTaskService = saveNewTaskService;
+        this.saveTaskService = saveTaskService;
+        this.existsTaskService = existsTaskService;
         this.validatorStrategyFactory = validatorStrategyFactory;
     }
 
-    public void create(TaskDTO dto) {
-        LOGGER.info("I=Criando nova task, type={}, dto={}", dto.type(), dto);
+    @Transactional
+    public void execute(TaskDTO dto) {
+        LOGGER.info("I=Creating a new task, type={}, dto={}", dto.type(), dto);
 
         final var validators = validatorStrategyFactory.getStrategyByType(dto.type());
 
         validators.forEach(validator -> validator.validate(dto));
 
-        saveNewTaskService.save(dto);
+        if (hasTaskWithOrder(dto)) {
+            LOGGER.info("I=Shifting up tasks order");
+            saveTaskService.shiftUpTaskOrders(dto);
+        }
+
+        saveTaskService.save(dto);
 
     }
+
+    private boolean hasTaskWithOrder(TaskDTO taskDTO) {
+        return existsTaskService.existsTaskByOrder(taskDTO.order());
+    }
+
 }
