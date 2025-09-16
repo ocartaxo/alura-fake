7941c47303ab74d30f5e785704af145fab864daa
Otavio Cartaxo <oacartaxo@gmail.com>
2025-09-16 15:49:23 -0300

    feat: handle custom validation exceptions

--- src/main/java/br/com/alura/AluraFake/util/ValidationExceptionHandler.java
+++ src/main/java/br/com/alura/AluraFake/util/ValidationExceptionHandler.java
@@ -1,5 +1,7 @@
 package br.com.alura.AluraFake.util;
 
+import br.com.alura.AluraFake.course.CourseValidationException;
+import br.com.alura.AluraFake.task.TaskValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.Collections;
 import java.util.List;
 
 @ControllerAdvice
@@ -17,5 +20,19 @@ public class ValidationExceptionHandler {
     public ResponseEntity<List<ErrorItemDTO>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         List<ErrorItemDTO> errors = ex.getBindingResult().getFieldErrors().stream().map(ErrorItemDTO::new).toList();
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(TaskValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<List<ErrorItemDTO>> handleValidationExceptions(TaskValidationException ex) {
+        final var error = new ErrorItemDTO(ex.getField(), ex.getMessage());
+        return ResponseEntity.badRequest().body(Collections.singletonList(error));
+    }
+
+    @ExceptionHandler(CourseValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<List<ErrorItemDTO>> handleValidationExceptions(CourseValidationException ex) {
+        final var error =  new ErrorItemDTO(ex.getField(), ex.getMessage());
+        return ResponseEntity.badRequest().body(Collections.singletonList(error));
     }
 }
