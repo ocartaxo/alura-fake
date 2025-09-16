c4eea7f843339e73e4b591f3f8c854e19193a927
Otavio Cartaxo <oacartaxo@gmail.com>
2025-09-16 00:53:45 -0300

    feat: add task repository methods

--- src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
+++ src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
@@ -1,6 +1,19 @@
 package br.com.alura.AluraFake.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    boolean existsByStatementIgnoringCase(String statement);
+
+    boolean existsTaskByOrder(int order);
+
+    @Modifying
+    @Query("UPDATE Task t SET t.order = t.order + 1 WHERE t.course.id = :courseId AND t.order >= :order")
+    void shiftOrdersUp(Long courseId, int order);
+
 }
