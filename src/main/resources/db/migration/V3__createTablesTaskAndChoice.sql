5767bd14642da4c9a7ec93318e90f3627e402cbe
Otavio Cartaxo <oacartaxo@gmail.com>
2025-09-15 21:07:11 -0300

    fix: change columns names to don't match with reserved keywords

--- src/main/resources/db/migration/V3__createTablesTaskAndChoice.sql
+++ src/main/resources/db/migration/V3__createTablesTaskAndChoice.sql
@@ -3,7 +3,7 @@ CREATE TABLE Task(
      task_type enum('OPEN_TEXT', 'SINGLE_CHOICE', 'MULTIPLE_CHOICE') CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci NOT NULL,
      statement varchar(255) NOT NULL,
-     `order` int NOT NULL,
+     task_order int NOT NULL,
      course_id bigint(20) NOT NULL,
      createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -16,13 +16,13 @@ CREATE TABLE Task(
 
 CREATE TABLE Choice(
      id bigint(20) NOT NULL AUTO_INCREMENT,
-     `option` varchar(80) NOT NULL,
+     choice_option varchar(80) NOT NULL,
      is_correct boolean NOT NULL,
      task_id bigint(20) NOT NULL,
      createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
      PRIMARY KEY (id),
-     CONSTRAINT FK_Option_Activity FOREIGN KEY (task_id) REFERENCES Task(id) ON DELETE CASCADE
-     CONSTRAINT UQ_Activity_Option UNIQUE (activity_id, `option`)
+     CONSTRAINT FK_Choice_Task FOREIGN KEY (task_id) REFERENCES Task(id) ON DELETE CASCADE,
+     CONSTRAINT UQ_Task_Choice UNIQUE (task_id, choice_option)
 
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
